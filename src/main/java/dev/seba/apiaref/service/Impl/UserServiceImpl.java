2f40e9003360b3464d3cad836dde60f35790976e
sebastian <sebastiandev.img@gmail.com>
2024-02-24 17:49:01 -0300

    service: se implementaron los metodos de buscar user por id, username y email

--- src/main/java/dev/seba/apiaref/service/Impl/UserServiceImpl.java
+++ src/main/java/dev/seba/apiaref/service/Impl/UserServiceImpl.java
@@ -1,23 +1,31 @@
 package dev.seba.apiaref.service.Impl;
 
+import dev.seba.apiaref.client.UserRestClient;
 import dev.seba.apiaref.model.User;
 import dev.seba.apiaref.service.IUserService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class UserServiceImpl implements IUserService {
+
+    private final UserRestClient userClient;
+
+    public UserServiceImpl(UserRestClient userClient){
+        this.userClient = userClient;
+    }
     @Override
     public User getUserById(int id) {
-        return null;
+        return userClient.findById(id);
     }
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        return userClient.findByUsername(username);
     }
 
     @Override
     public User getUserByEmail(String email) {
-        return null;
+        return userClient.findByEmail(email);
     }
 }
