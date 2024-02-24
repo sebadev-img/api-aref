5409fa389f020620681b6f625c7861117b71f6b6
sebastian <sebastiandev.img@gmail.com>
2024-02-24 17:54:12 -0300

    service: se creo el dto para listado de users

---
+++ src/main/java/dev/seba/apiaref/dto/response/UsersResponseDto.java
@@ -0,0 +1,10 @@
+package dev.seba.apiaref.dto.response;
+
+import dev.seba.apiaref.model.User;
+
+import java.util.List;
+
+public class UsersResponseDto {
+    int count;
+    List<User> results;
+}
