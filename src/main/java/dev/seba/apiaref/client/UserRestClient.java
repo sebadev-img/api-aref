40742faab7afbbd6b8d628de1fe3709c96dc8551
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2024-02-24 17:22:32 -0300

    Feature/userClient (#2)
    * se creo el modelo para company
    
    * se creo el model para geo
    
    * se creo el model para address
    
    * se creo el model para user
    
    * client: se creo la funcionalidad de obtener users de la api externa
    
    * client: se creo la funcionalidad de obtener usuario por id de la api externa
    
    * client: se creo la funcionalidad de obtener usuario por username de la api externa
    
    * client:se creo la funcionalidad de obtener usuario por email de la api externa
    
    * client:se agrego la funcionalidad de buscar usuarios por ciudad de la api externa
    
    * client: se creo el test para buscar todos los usuarios
    
    * client: se creo el test para buscar usuario por id
    
    * client:se creo el test para buscar usuario por username
    
    * client:se corrigio el nombre de un test
    
    * client: se creo el test para buscar user por email
    
    * client: se agrego el test de la funcion buscar usuarios por ciudad

---
+++ src/main/java/dev/seba/apiaref/client/UserRestClient.java
@@ -0,0 +1,55 @@
+package dev.seba.apiaref.client;
+
+import dev.seba.apiaref.model.User;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class UserRestClient {
+
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder){
+        this.restClient = builder
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+    }
+
+    public List<User> findAll(){
+        return restClient.get()
+                .uri("/users")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<User>>() {});
+    }
+
+    public User findById(int id){
+        return restClient.get()
+                .uri("/users/{id}",id)
+                .retrieve()
+                .body(User.class);
+    }
+
+    public User findByUsername(String username){
+        return restClient.get()
+                .uri("/users?username={username}",username)
+                .retrieve()
+                .body(User.class);
+    }
+
+    public User findByEmail(String email){
+        return restClient.get()
+                .uri("/users?email={email}",email)
+                .retrieve()
+                .body(User.class);
+    }
+
+    public List<User> findByCity(String city){
+        return restClient.get()
+                .uri("/users?address.city={city}",city)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<User>>() {});
+    }
+}
