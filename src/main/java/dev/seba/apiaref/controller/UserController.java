186dd5d7b1402b55046293b8b65b090379e2837e
sebastian <sebastiandev.img@gmail.com>
2024-02-24 20:26:56 -0300

    controller: se agrego el controller de user

---
+++ src/main/java/dev/seba/apiaref/controller/UserController.java
@@ -0,0 +1,52 @@
+package dev.seba.apiaref.controller;
+
+import dev.seba.apiaref.dto.response.UsersResponseDto;
+import dev.seba.apiaref.model.User;
+import dev.seba.apiaref.service.IUserService;
+import dev.seba.apiaref.service.Impl.UserServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(UserServiceImpl userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<UsersResponseDto> getAll(){
+        UsersResponseDto response = userService.getAll();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id){
+        User response = userService.getUserById(id);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<User> searchUser(@RequestParam(required = false) String username, @RequestParam(required = false) String email){
+        User response;
+        if(username != null){
+            response = userService.getUserByUsername(username);
+        } else if (email != null) {
+            response = userService.getUserByEmail(email);
+        }
+        else{
+            response = null;
+        }
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping("/city/{name}")
+    public ResponseEntity<UsersResponseDto> getUsersByCity(@PathVariable String name){
+        UsersResponseDto response = userService.getUsersByCity(name);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+}
