351c352db0613177b4c1f95f866937b48d5ec88e
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2024-02-25 02:40:56 -0300

    Metric Service (#15)
    * service: se creo el dto de post metric y el metodo de metrica dde post
    
    * service: se creo el test para el metodo metrica de post

--- src/main/java/dev/seba/apiaref/service/Impl/MetricServiceImpl.java
+++ src/main/java/dev/seba/apiaref/service/Impl/MetricServiceImpl.java
@@ -1,7 +1,10 @@
 package dev.seba.apiaref.service.Impl;
 
+import dev.seba.apiaref.client.CommentRestClient;
 import dev.seba.apiaref.client.PostRestClient;
+import dev.seba.apiaref.dto.response.PostMetricsResponseDto;
 import dev.seba.apiaref.dto.response.UserMetricsResponseDto;
+import dev.seba.apiaref.model.Comment;
 import dev.seba.apiaref.model.Post;
 import dev.seba.apiaref.service.IMetricService;
 import org.springframework.stereotype.Service;
@@ -12,9 +15,12 @@ import java.util.List;
 public class MetricServiceImpl implements IMetricService {
 
     private final PostRestClient postClient;
+    private final CommentRestClient commentClient;
 
-    public MetricServiceImpl(PostRestClient postClient){
+    public MetricServiceImpl(PostRestClient postClient, CommentRestClient commentClient){
+
         this.postClient = postClient;
+        this.commentClient = commentClient;
     }
     @Override
     public UserMetricsResponseDto getUserMetrics(int userId) {
@@ -24,6 +30,16 @@ public class MetricServiceImpl implements IMetricService {
         metricDto.setPostCount(userPosts.size());
         metricDto.setUserInfoPath("/api/users/"+userId);
         metricDto.setUserPostsPath("/api/posts/search?userId="+userId);
+        return metricDto;
+    }
+
+    @Override
+    public PostMetricsResponseDto getPostMetrics(int postId) {
+        List<Comment> postComments = commentClient.findCommentsByPostId(postId);
+        PostMetricsResponseDto metricDto = new PostMetricsResponseDto();
+        metricDto.setPostId(postId);
+        metricDto.setCommentsCount(postComments.size());
+        metricDto.setPostInfoPath("/api/posts/"+postId);
         return metricDto;
     }
 }
--- src/test/java/dev/seba/apiaref/service/Impl/MetricServiceImplTest.java
+++ src/test/java/dev/seba/apiaref/service/Impl/MetricServiceImplTest.java
@@ -1,9 +1,13 @@
 package dev.seba.apiaref.service.Impl;
 
+import dev.seba.apiaref.client.CommentRestClient;
 import dev.seba.apiaref.client.PostRestClient;
+import dev.seba.apiaref.dto.response.PostMetricsResponseDto;
 import dev.seba.apiaref.dto.response.UserMetricsResponseDto;
+import dev.seba.apiaref.model.Comment;
 import dev.seba.apiaref.model.Post;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -20,6 +24,9 @@ class MetricServiceImplTest {
     @Mock
     private PostRestClient postClient;
 
+    @Mock
+    private CommentRestClient commentClient;
+
     @InjectMocks
     private MetricServiceImpl metricService;
 
@@ -32,5 +39,16 @@ class MetricServiceImplTest {
         when(postClient.findByUserId(1)).thenReturn(data);
         UserMetricsResponseDto metricDto = metricService.getUserMetrics(1);
         assertEquals(2, metricDto.getPostCount());
+    }
+
+    @Test
+    void testThatGetPostMetricsReturnsMetricDto(){
+        List<Comment> data = List.of(
+                new Comment(1,1,"name","email","body"),
+                new Comment(2,1,"name","email","body")
+        );
+        when(commentClient.findCommentsByPostId(1)).thenReturn(data);
+        PostMetricsResponseDto metricDto = metricService.getPostMetrics(1);
+        assertEquals(2,metricDto.getCommentsCount());
     }
 }
