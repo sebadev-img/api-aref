49d9e4f85a4bed6cd857e660475f69b99be34ccd
sebastian <sebastiandev.img@gmail.com>
2024-02-25 14:37:14 -0300

    service: se implemento el metodo de obtener el top 10 de post

--- src/main/java/dev/seba/apiaref/dto/response/PostReportResponseDto.java
+++ src/main/java/dev/seba/apiaref/dto/response/PostReportResponseDto.java
@@ -3,9 +3,10 @@ package dev.seba.apiaref.dto.response;
 import dev.seba.apiaref.dto.PostMetric;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PostReportResponseDto {
     Integer count;
-    PostMetric postMetric;
-
+    List<PostMetric> results;
 }
