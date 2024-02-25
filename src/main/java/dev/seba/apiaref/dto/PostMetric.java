67a5fa72bef33b6ea81d5c95846bafb2a002174f
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2024-02-25 16:23:01 -0300

    Report Controller (#18)
    * controller/ se creo el controller para los reportes
    
    * controller:se creo el test para el report controller

--- src/main/java/dev/seba/apiaref/dto/PostMetric.java
+++ src/main/java/dev/seba/apiaref/dto/PostMetric.java
@@ -1,8 +1,12 @@
 package dev.seba.apiaref.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostMetric {
     Integer postId;
     Integer commentCount;
