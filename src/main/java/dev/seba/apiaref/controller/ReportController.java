1922627ed1da528f1b15a10f6f67f15e07cd959a
sebastian <sebastiandev.img@gmail.com>
2024-02-25 16:04:36 -0300

    controller/ se creo el controller para los reportes

---
+++ src/main/java/dev/seba/apiaref/controller/ReportController.java
@@ -0,0 +1,29 @@
+package dev.seba.apiaref.controller;
+
+import dev.seba.apiaref.dto.response.PostReportResponseDto;
+import dev.seba.apiaref.dto.response.PostsResponseDto;
+import dev.seba.apiaref.service.IReportService;
+import dev.seba.apiaref.service.Impl.ReportServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/reports")
+public class ReportController {
+
+    private final IReportService reportService;
+
+    public ReportController(ReportServiceImpl reportService){
+        this.reportService = reportService;
+    }
+
+    @GetMapping("/post/top")
+    public ResponseEntity<PostReportResponseDto> getTop10Post(){
+        PostReportResponseDto response = reportService.getTop10PostByComment();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
